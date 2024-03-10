69e427b9b5c34af278311fa168188eca9686be69
Diego Romero <141085730+diegmero@users.noreply.github.com>
2024-03-10 12:56:31 -0500

    Se agrega el Primer Commit

---
+++ CURSO.md
@@ -0,0 +1,11 @@
+# INSTRUCCIONES
+
+- Cada vez que veas una cápsula en la plataforma:
+
+  1. Al terminar de verla, accede a los ejercicios (ejercicios.js) e intenta hacerlos de manera personal.
+  2. Revisa los contenidos en el manual de trabajo de esa cápsula.
+  3. Comenta tus resultados en los comentarios.
+
+- Disfruta el curso.
+
+- Escríbeme cualquier inquietud: m@mikenieva.com
---
+++ M1-C3-variables/ejercicios.js
@@ -0,0 +1,87 @@
+/*
+  NETZUN.COM
+  INSTRUCTOR: MIKE NIEVA
+  JAVASCRIPT BÁSICO
+  
+  MÓDULO 1
+  CÁPSULA 3. "VARIABLES, TIPOS DE DATOS Y OPERADORES"
+*/
+
+//============EJERCICIOS============//
+//==================================//
+//==================================//
+
+/* EJERCICIO 1: CREA UNA VARIABLE DE CADA TIPO DE DATO E IMPRÍMELA EN CONSOLA
+- Booleano
+- null
+- undefined
+- Number
+- String
+*/
+
+// Booleano
+
+let edadMaxima = false;
+console.log(edadMaxima);
+
+
+//Null
+
+let casa = null;
+console.log(casa);
+
+//Undefined
+
+let tiempo = undefined;
+console.log(tiempo)
+
+
+//Number
+
+let edad = 15;
+console.log("La edad que tengo es " + edad);
+
+//Dato String
+
+let empresa = ("GaluikCorp");
+console.log(empresa);
+
+
+//==================================//
+/* EJERCICIO 2: CREA 4 VARIABLES E IMPRÍMELAS EN CONSOLA
+- Una variable que sume dos números
+- Una variable que reste dos números
+- Una variable que multiplique dos núúmeros
+- Una variable que divida dos núúmeros
+*/
+
+
+// ***************** EJERCICIO 2
+
+// - suma de dos números
+
+let num1 = 15 + 75;
+console.log("La suma de todo es = " + num1);
+
+// resta
+
+let resta = 500 - 100;
+console.log(resta);
+
+// multiplicacion
+
+let mult = 10 * 10;
+console.log("el resultado de multiplicacion es = " + mult);
+
+//division
+
+let division = 200 / 5;
+console.log("la division es de = " + division);
+
+
+//==================================//
+/* EJERCICIO 3: CREA 3 VARIABLES E IMPRÍMELAS EN CONSOLA
+- Crea una variable que almacene un primer número. Luego, otra variable que almacene un segundo número, finalmente que se sumen por referencia.
+*/
+
+
